4ccdba20eda603253b880f8b4a03cd94a25f3438
eunsikim <kimeunsik229@gmail.com>
2026-02-10 13:38:00 -0500

    in class 02/10

---
+++ Week 5/fizzBuzz.py
@@ -0,0 +1,19 @@
+def main():
+    n = int(input("Enter a number: "))
+
+    i = 1
+
+    while i <= n:
+        if i % 3 == 0 and i % 5 == 0:
+            print("FizzBuzz")
+        elif i % 3 == 0:
+            print("Fizz")
+        elif i % 5 == 0:
+            print("Buzz")
+        else:
+            print(i)
+
+        i += 1
+
+if __name__ == "__main__":
+    main()
