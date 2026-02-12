3c75abb0ac9aa3f81b33c92d90b47c904f48f9b4
eunsikim <kimeunsik229@gmail.com>
2026-02-12 13:42:12 -0500

    in class 02/12

--- Week 5/in_class_1.py
+++ Week 5/in_class_1.py
@@ -10,15 +10,22 @@ def main():
     password = input("Enter a password: ")
 
     hasNumber = False
+    has8Chars = False
 
     for c in password:
         if c == "0" or c == "1" or c == "2" or c == "3" or c == "4" or c == "5" or c == "6" or c == "7" or c == "8" or c == "9":
             hasNumber = True
             break
-
-    for ...
     
-    if hasNumber:
+    counter = 0
+
+    for c in password:
+        counter += 1
+    
+    if counter >= 8:
+        has8Chars = True
+    
+    if hasNumber == True and has8Chars == True:
         print("The password is valid")
     else:
         print("The password is not valid")
