b6b38005f61120663bef14a4ee18f832d0bf7f6d
eunsikim <kimeunsik229@gmail.com>
2026-03-24 12:43:01 -0400

    in class 3/24

---
+++ Week 10/in_class_2.py
@@ -0,0 +1,12 @@
+# Create a function where you take a single list (float) as parameter
+# and then figures out the smalles element in the list and then
+# return that value.
+#
+# Example:
+#   [3, 4, 10, 1], the function should return 1
+
+def main():
+    pass
+
+if __name__ == "__main__":
+    main()
